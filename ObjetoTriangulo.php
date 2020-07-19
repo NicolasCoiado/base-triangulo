abee7e8e287eabfacf16a6500db59e495151d015
Nicolas Mauricio Martins Coiado <48792614+NicolasCoiado03@users.noreply.github.com>
2020-07-19 13:09:08 -0300

    Delete ObjetoTriangulo.php

--- ObjetoTriangulo.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once 'Triangulo.php'; //Importando o arquivo com a classe Triangulo;
-
-        $triangulo1 = new Triangulo();
-        $triangulo1->setAltura($_GET['alt']);
-        $triangulo1->setBase($_GET['base']);
-        $triangulo1->imprimir();
-    
-?>
